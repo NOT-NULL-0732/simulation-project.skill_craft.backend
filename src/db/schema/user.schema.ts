fa6bac71b54439d0e106a391395526c4e665963d
NOT-NULL-0732 <2662631731@qq.com>
2025-06-23 14:58:36 +0800

    修改user.schema.ts

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -3,7 +3,9 @@ import { char, integer, pgTable, timestamp } from 'drizzle-orm/pg-core';
 export const userSchema = pgTable('user', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: char({ length: 50 }).notNull(),
-  user_name: timestamp().notNull(),
+  user_name: timestamp(),
+  password: char({ length: 200 }).notNull(),
+  user_token: char({ length: 255 }),
   updated_at: timestamp({ mode: 'string' }).$onUpdateFn(() =>
     new Date().toString(),
   ),
