f78e3bb2f58a3b3e84f3464043966097a6ed339f
NOT-NULL-0732 <2662631731@qq.com>
2025-06-23 20:50:20 +0800

    将user.schema.ts的email设置成唯一

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -2,7 +2,7 @@ import { char, integer, pgTable, timestamp } from 'drizzle-orm/pg-core';
 
 export const userSchema = pgTable('user', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  email: char({ length: 50 }).notNull(),
+  email: char({ length: 50 }).notNull().unique(),
   user_name: timestamp(),
   password: char({ length: 200 }).notNull(),
   user_token: char({ length: 255 }),
