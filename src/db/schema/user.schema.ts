9312fe62e425a1a44c3300c9b32c7414b52eb195
NOT-NULL-0732 <2662631731@qq.com>
2025-06-23 21:07:32 +0800

    修复用户名字段类型

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -3,7 +3,7 @@ import { char, integer, pgTable, timestamp } from 'drizzle-orm/pg-core';
 export const userSchema = pgTable('user', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: char({ length: 50 }).notNull().unique(),
-  user_name: timestamp(),
+  user_name: char({ length: 50 }).notNull(),
   password: char({ length: 200 }).notNull(),
   user_token: char({ length: 255 }),
   updated_at: timestamp({ mode: 'string' }).$onUpdateFn(() =>
