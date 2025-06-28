8212e5f403362d83b5c9674620bd9c6912208df3
NOT-NULL-0732 <2662631731@qq.com>
2025-06-29 03:35:21 +0800

    不想写提交日志了，这个项目先一把梭了，以后再说

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
+import { AppConfig } from '@/common/config/index.config';
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema/*',
   dbCredentials: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'root',
-    password: 'root',
-    database: 'example',
+    host: AppConfig.db.host,
+    port: AppConfig.db.port,
+    user: AppConfig.db.user,
+    password: AppConfig.db.password,
+    database: AppConfig.db.database,
     ssl: false,
   },
   schemaFilter: ['public'],
