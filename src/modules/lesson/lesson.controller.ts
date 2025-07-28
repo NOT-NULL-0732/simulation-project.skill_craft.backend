22f41c8cdaefc4f662bb34543c87fc36390e2330
NOT-NULL-0732 <2662631731@qq.com>
2025-07-29 01:52:06 +0800

    开始重写课程api逻辑

--- src/modules/lesson/lesson.controller.ts
+++ src/modules/lesson/lesson.controller.ts
@@ -1,13 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { DrizzleService } from '@/modules/drizzle/drizzle.service';
-import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 @Controller('lesson')
-export class LessonController {
-  db: NodePgDatabase<typeof import('../../db/schema/index.schema')>;
-
-  constructor(private readonly drizzleService: DrizzleService) {
-    const { db } = this.drizzleService;
-    this.db = db;
-  }
-}
+export class LessonController {}
--- src/modules/lesson/lesson.service.ts
+++ src/modules/lesson/lesson.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DrizzleService } from '@/modules/drizzle/drizzle.service';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  db: NodePgDatabase<typeof import('../../db/schema/index.schema')>;
+
+  constructor(private readonly drizzleService: DrizzleService) {
+    const { db } = this.drizzleService;
+    this.db = db;
+  }
+}
