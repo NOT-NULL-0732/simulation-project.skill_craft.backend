8bd1a883f709b2546e69d851757eb360b8b77cd7
NOT-NULL-0732 <2662631731@qq.com>
2025-09-10 18:50:06 +0800

    不想写提交日志

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { LessonModule } from './modules/lesson/lesson.module';
 import { FileModule } from './modules/file/file.module';
 import { RedisModule } from './modules/redis/redis.module';
-import { RequestTestMiddleware } from '@/common/middleware/request-test.middleware';
 
 @Module({
   imports: [AuthModule, LessonModule, FileModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    consumer.apply(RequestTestMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
