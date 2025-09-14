c806a8c1d992414e6dd061c3700818907071ce1d
NOT-NULL-0732 <2662631731@qq.com>
2025-09-14 23:55:40 +0800

    准备重写鉴权模块

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,121 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '@/modules/auth/auth.service';
 import { ZodValidationPipe } from 'nestjs-zod';
-import { z } from 'zod';
 import { createResponse } from '@/common/utils/create-response';
 import { ResponseStatusCode } from '@/common/types/response-status.enum';
-import {
-  CreatePermissionZSchema,
-  CreateRolePermissionZSchema,
-  CreateRoleZSchema,
-  CreateUserRoleZSchema,
-  DeleteRolePermissionZSchema,
-  DeleteUserRoleZSchema,
-  LoginZSchema,
-  RegisterZSchema,
-} from '@/modules/auth/auth.z-schema';
+import { LoginZSchema } from '@/modules/auth/auth.z-schema';
 import { AuthPermission } from '@/common/decorator/permission.decorator';
+import { TypeAuthController } from '@/modules/auth/auth.type';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @AuthPermission('AUTH:USER:REGISTER', false)
-  @Post('register')
-  async register(
-    @Body(new ZodValidationPipe(RegisterZSchema))
-    body: z.infer<typeof RegisterZSchema>,
-  ) {
-    await this.authService.register(body);
-  }
-
   @AuthPermission('AUTH:USER:LOGIN', false)
   @Post('login')
   async login(
     @Body(new ZodValidationPipe(LoginZSchema))
-    body: z.infer<typeof LoginZSchema>,
+    body: TypeAuthController['login']['body'],
   ) {
     const loginResult = await this.authService.login(body);
     return createResponse(ResponseStatusCode.REQUEST_SUCCESS, loginResult);
-  }
-
-  @AuthPermission('AUTH:ROLE:GET')
-  @Get('role')
-  async getRoles() {
-    return createResponse(
-      ResponseStatusCode.REQUEST_SUCCESS,
-      await this.authService.getRoles(),
-    );
-  }
-
-  @AuthPermission('AUTH:ROLE:ADD')
-  @Post('role')
-  async createRole(
-    @Body(new ZodValidationPipe(CreateRoleZSchema))
-    body: z.infer<typeof CreateRoleZSchema>,
-  ) {
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS, {
-      role_id: await this.authService.createRole(body),
-    });
-  }
-
-  @AuthPermission('AUTH:USER_ROLE:ADD')
-  @Post('user-role')
-  async createUserRole(
-    @Body(new ZodValidationPipe(CreateUserRoleZSchema))
-    body: z.infer<typeof CreateUserRoleZSchema>,
-  ) {
-    await this.authService.createUserRole(body);
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS);
-  }
-
-  @AuthPermission('AUTH:USER_ROLE:ADD')
-  @Post('user-role')
-  async deleteUserRole(
-    @Body(new ZodValidationPipe(DeleteUserRoleZSchema))
-    body: z.infer<typeof DeleteUserRoleZSchema>,
-  ) {
-    await this.authService.deleteUserRole(body);
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS);
-  }
-
-  @AuthPermission('AUTH:PERMISSION:GET')
-  @Get('permission')
-  async getPermissions() {
-    return createResponse(
-      ResponseStatusCode.REQUEST_SUCCESS,
-      await this.authService.getPermissions(),
-    );
-  }
-
-  @AuthPermission('AUTH:PERMISSION:ADD')
-  @Post('permission')
-  async createPermission(
-    @Body(new ZodValidationPipe(CreatePermissionZSchema))
-    body: z.infer<typeof CreatePermissionZSchema>,
-  ) {
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS, {
-      permission_id: await this.authService.createPermission(body),
-    });
-  }
-
-  @AuthPermission('AUTH:ROLE_PERMISSION:ADD')
-  @Post('role-permission')
-  async createRolePermission(
-    @Body(new ZodValidationPipe(CreateRolePermissionZSchema))
-    body: z.infer<typeof CreateRolePermissionZSchema>,
-  ) {
-    await this.authService.createRolePermission(body);
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS);
-  }
-
-  @AuthPermission('AUTH:ROLE_PERMISSION:DELETE')
-  @Post('role-permission')
-  async deleteRolePermission(
-    @Body(new ZodValidationPipe(DeleteRolePermissionZSchema))
-    body: z.infer<typeof DeleteRolePermissionZSchema>,
-  ) {
-    await this.authService.deleteRolePermission(body);
-    return createResponse(ResponseStatusCode.REQUEST_SUCCESS);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,22 +5,7 @@ import { and, eq } from 'drizzle-orm';
 import { z } from 'zod';
 import { BusinessException } from '@/common/exception/business.exception';
 import { ResponseStatusCode } from '@/common/types/response-status.enum';
-import {
-  CreatePermissionZSchema,
-  CreateRolePermissionZSchema,
-  CreateRoleZSchema,
-  CreateUserRoleZSchema,
-  DeleteRolePermissionZSchema,
-  DeleteUserRoleZSchema,
-  LoginZSchema,
-  RegisterZSchema,
-} from '@/modules/auth/auth.z-schema';
-// @ts-ignore
-import { DrizzleQueryError } from 'drizzle-orm/errors';
-import { roleSchema } from '@/db/schema/role.schema';
-import { permissionSchema } from '@/db/schema/permission.schema';
-import { userRoleSchema } from '@/db/schema/user-role.schema';
-import { rolePermissionSchema } from '@/db/schema/role-permission.schema';
+import { LoginZSchema } from '@/modules/auth/auth.z-schema';
 import db from '@/db';
 import { CryptoService } from '@/modules/crypto/crypto.service';
 import { LoginTokenData } from '@/modules/auth/auth.type';
@@ -31,23 +16,6 @@ export class AuthService {
 
   _hashPassword(password: string): string {
     return crypto.createHash('sha256').update(password).digest('hex');
-  }
-
-  async register(data: z.infer<typeof RegisterZSchema>) {
-    try {
-      await db.insert(userSchema).values({
-        user_name: data.user_name,
-        password: this._hashPassword(data.password),
-        email: data.email,
-      });
-    } catch (err: unknown) {
-      if (err instanceof DrizzleQueryError && err) {
-        throw new BusinessException(
-          ResponseStatusCode.COMMON_INSERT_UNIQUE_ERROR,
-          '用户名或邮箱重复',
-        );
-      }
-    }
   }
 
   async login(data: z.infer<typeof LoginZSchema>) {
@@ -97,111 +65,6 @@ export class AuthService {
         ResponseStatusCode.AUTH__TOKEN_VALIDATOR_ERROR,
       );
     return userId;
-  }
-
-  async getRoles() {
-    return db.query.roleSchema.findMany();
-  }
-
-  async createRole(body: z.infer<typeof CreateRoleZSchema>): Promise<string> {
-    const insertResult = await db
-      .insert(roleSchema)
-      .values({
-        name: body.name,
-        role_key: body.role_key,
-        description: body.description,
-      })
-      .onConflictDoNothing({
-        target: [roleSchema.role_key],
-      })
-      .returning({ id: roleSchema.id });
-    if (insertResult.length === 0)
-      throw new BusinessException(
-        ResponseStatusCode.AUTH__ROLE_IDENTIFICATION_REPEAT_ERROR,
-      );
-    return insertResult[0].id;
-  }
-
-  async getPermissions() {
-    return db.query.permissionSchema.findMany();
-  }
-
-  async createUserRole(body: z.infer<typeof CreateUserRoleZSchema>) {
-    const insertResult = await db
-      .insert(userRoleSchema)
-      .values({
-        ...body,
-      })
-      .onConflictDoNothing({
-        target: [userRoleSchema.user_id, userRoleSchema.role_id],
-      });
-    if (insertResult.rowCount === 0)
-      throw new BusinessException(
-        ResponseStatusCode.AUTH__REPEAT_ADD_ROLE_WITH_USER_ERROR,
-      );
-  }
-
-  async deleteUserRole(body: z.infer<typeof DeleteUserRoleZSchema>) {
-    await db
-      .delete(userRoleSchema)
-      .where(
-        and(
-          eq(userRoleSchema.user_id, body.user_id),
-          eq(userRoleSchema.role_id, body.role_id),
-        ),
-      );
-  }
-
-  async createRolePermission(
-    body: z.infer<typeof CreateRolePermissionZSchema>,
-  ) {
-    const insertResult = await db
-      .insert(rolePermissionSchema)
-      .values({ ...body })
-      .onConflictDoNothing({
-        target: [
-          rolePermissionSchema.role_id,
-          rolePermissionSchema.permission_id,
-        ],
-      });
-    if (insertResult.rowCount === 0)
-      throw new BusinessException(
-        ResponseStatusCode.AUTH__REPEAT_ADD_PERMISSION_WITH_ROLE_ERROR,
-      );
-  }
-
-  async deleteRolePermission(
-    body: z.infer<typeof DeleteRolePermissionZSchema>,
-  ) {
-    await db
-      .delete(rolePermissionSchema)
-      .where(
-        and(
-          eq(rolePermissionSchema.role_id, body.role_id),
-          eq(rolePermissionSchema.permission_id, body.permission_id),
-        ),
-      );
-  }
-
-  async createPermission(
-    body: z.infer<typeof CreatePermissionZSchema>,
-  ): Promise<string> {
-    const insertResult = await db
-      .insert(permissionSchema)
-      .values({
-        name: body.name,
-        permission_key: body.permission_key,
-        description: body.description,
-      })
-      .onConflictDoNothing({
-        target: [permissionSchema.permission_key],
-      })
-      .returning({ id: permissionSchema.id });
-    if (insertResult.length === 0)
-      throw new BusinessException(
-        ResponseStatusCode.AUTH__PERMISSION_IDENTIFICATION_REPEAT_ERROR,
-      );
-    return insertResult[0].id;
   }
 
   private _genRandomToken(userId: string, date: number) {
