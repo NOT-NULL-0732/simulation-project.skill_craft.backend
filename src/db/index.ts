ee66624caea1f23bb27a3ae8a0219f9ec0af452a
NOT-NULL-0732 <2662631731@qq.com>
2025-09-27 00:08:39 +0800

    feat: 添加数据库初始化状态日至

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,22 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@/db/schema/index.schema';
 import { AppConfig } from '@/common/config/index.config';
+import { sql } from 'drizzle-orm';
+import { Logger } from '@nestjs/common';
 
 const db = drizzle({
   connection: AppConfig.db.connectionString,
   logger: true,
   schema,
 });
+
+db.execute(sql`select 1;`)
+  .then(() => {
+    Logger.verbose('数据库已连接');
+  })
+  .catch((err) => {
+    Logger.error('数据库连接失败');
+    console.log(err);
+  });
 
 export default db;
